ea6c1e37de01434e8990c8b2c882aef48c55748b
José Claudio da Silva Júnior (CESAR) <jcsj2@cesar.org.br>
2022-11-04 11:03:42 -0300

    Add treino para modulos

--- 09-npm/index.js
+++ 09-npm/index.js
@@ -1,8 +1,9 @@
 const axios = require('axios')
+const fs = require('fs')
 
 // https://pokeapi.co/
 // pokemon - https://pokeapi.co/api/v2/pokemon/pikachu
 
 axios.create().get('https://api.github.com/users/JoseClaudioADS').then((dados) => {
-    console.log(dados.data)
+    fs.writeFileSync('arquivo.txt', JSON.stringify(dados.data))
 })
