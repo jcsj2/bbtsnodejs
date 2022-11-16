bbda885d90b6798fff6b45db3adba38c635f8b4b
José Claudio da Silva Júnior (CESAR) <jcsj2@cesar.org.br>
2022-11-16 10:49:17 -0300

    Add modules

---
+++ 12-modules/calculadora.js
@@ -0,0 +1,30 @@
+/*export class Cliente {
+
+}*/
+
+function soma(numero1, numero2) {
+    return numero1 + numero2
+}
+
+export function subtracao(numero1, numero2) {
+    return numero1 - numero2
+}
+
+export function multiplicacao(numero1, numero2) {
+    return numero1 * numero2
+}
+
+function divisao(numero1, numero2) {
+    return numero1 / numero2
+}
+
+//CommonJS modules
+//module.exports = 
+
+//ECMAScript Modules
+/*export default {
+    soma,
+    subtracao,
+    multiplicacao,
+    divisao
+}*/
---
+++ 12-modules/index.js
@@ -0,0 +1,6 @@
+import { multiplicacao } from './calculadora.js'
+
+console.log(multiplicacao(10, 9))
+
+//console.log(calculadora.soma(1, 2))
+//console.log(calculadora.subtracao(5, 2))
