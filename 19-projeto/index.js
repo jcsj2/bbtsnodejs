52a5c011ecafe3067dc85153dfe69d341451708b
José Claudio da Silva Júnior (CESAR) <jcsj2@cesar.org.br>
2022-11-30 10:26:34 -0300

    Add projeto

--- 19-projeto/index.js
+++ 19-projeto/index.js
@@ -5,6 +5,23 @@ const fs = require('fs')
 const app = express()
 app.use(express.json())
 
-app.listen(3000, () => {
+const dadosColetados = []
+
+
+app.get('/buscarPorCodigo/:codigoFipe', (req, res) => {
+    const { codigoFipe } = req.params
+    axios.create().get(`https://brasilapi.com.br/api/fipe/preco/v1/${codigoFipe}`).then((resultado) => {
+        const primeiroConjuntoDeDados = resultado.data[0]
+        dadosColetados.push(primeiroConjuntoDeDados)
+        res.json(primeiroConjuntoDeDados)
+    })  
+})
+
+app.post('/gerarArquivo', (req, res) => {
+    fs.writeFileSync('dadosFipe.txt', JSON.stringify(dadosColetados))
+    res.json(dadosColetados)
+})
+
+app.listen(3005, () => {
     console.log('Servidor iniciado')
 })
